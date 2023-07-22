8508dddb16a32df2e6d3a3c59803c53a5b9a5195
Zhukov Nikita <nypno@mail.ru>
2023-07-22 19:19:20 +0300

    added solution first task

---
+++ jcore/hw2/Task1.java
@@ -0,0 +1,43 @@
+package ru.gb.jcore.hw2;
+
+import java.util.Arrays;
+
+public class Task1 {
+
+    /*
+    Доделать сортировку подсчетом с урока (если нет, реализовать) классическим способом через промежуточный массив
+     */
+
+    public static void main(String[] args) {
+        int[] arr = {0, -2, 3, 1, 0, 0, 1};
+        System.out.println(Arrays.toString(countingSort(arr)));
+    }
+
+    public static int[] countingSort(int[] array) {
+        int[] sortedArr = new int[array.length];
+
+        int min = 0;
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        int[] temp = new int[max - min + 1];
+        for (int i = 0; i < array.length; i++) {
+            temp[array[i] - min]++;
+        }
+        System.out.println(Arrays.toString(temp));
+        int index = 0;
+        for (int i = 0; i < temp.length; i++) {
+            for (int j = temp[i]; j > 0; j--) {
+                sortedArr[index] = i + min;
+                index++;
+            }
+        }
+        return sortedArr;
+    }
+}
